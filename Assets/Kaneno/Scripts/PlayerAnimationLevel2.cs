df07899c15139fb69bcd34396ad7659259e61b58
ftvoid <gensaicuttlefish@gmail.com>
2023-02-04 19:03:40 +0900

    動きを仮実装。

---
+++ Assets/Kaneno/Scripts/PlayerAnimationLevel2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAnimationLevel2 : PlayerAnimationBase
+    {
+        [SerializeField] private Transform _body;
+
+        [SerializeField] private Transform _cell;
+
+        private Vector2 _prevPosition;
+        private float _currentAngle;
+        private float _currentAngleVelocity;
+
+        private void Update()
+        {
+            var pos = (Vector2) _body.position;
+            var delta = pos - _prevPosition;
+            if (delta != Vector2.zero)
+            {
+                var angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+
+                _currentAngle = Mathf.SmoothDampAngle(
+                    _currentAngle,
+                    angle,
+                    ref _currentAngleVelocity,
+                    0.1f
+                );
+
+                _body.rotation = Quaternion.Euler(0, 0, _currentAngle);
+            }
+
+            _prevPosition = pos;
+        }
+    }
+}
