b58e2fd4639cca70c13fa77dc8da5df465916f65
ftvoid <gensaicuttlefish@gmail.com>
2023-02-05 00:14:31 +0900

    第３形態の向きを変える動作を仮実装。

---
+++ Assets/Kaneno/Scripts/PlayerAnimationTop.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAnimationTop : PlayerAnimationBase
+    {
+        [SerializeField] private Transform _body;
+        [SerializeField] private float _angleOffset = 0;
+
+        private Vector2 _prevPosition;
+        private float _currentAngle;
+        private float _currentAngleVelocity;
+
+        private void Update()
+        {
+            var pos = (Vector2) _body.position;
+            var delta = pos - _prevPosition;
+            if (delta != Vector2.zero)
+            {
+                var angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+
+                _currentAngle = Mathf.SmoothDampAngle(
+                    _currentAngle,
+                    angle + _angleOffset,
+                    ref _currentAngleVelocity,
+                    0.3f
+                );
+
+                _body.rotation = Quaternion.Euler(0, 0, _currentAngle);
+            }
+
+            _prevPosition = pos;
+        }
+    }
+}
