eeb5302f35fdc9038cdb82172043ea708690f2d5
ftvoid <gensaicuttlefish@gmail.com>
2023-02-04 23:03:26 +0900

    不要なログ出力、コメント整理。

--- Assets/Kaneno/Scripts/PlayerCollision.cs
+++ Assets/Kaneno/Scripts/PlayerCollision.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Player
@@ -7,18 +6,16 @@ namespace Player
     {
         private void OnTriggerEnter2D(Collider2D col)
         {
-            print("OnTriggerEnter2D");
-
-            // TODO : アイテム、敵などの当たり判定
-
-            // 一旦は全て餌とみなして仮実装
+            // 餌かどうかの判定処理
             var feed = col.GetComponent<FeedBase>();
             if (feed != null)
             {
+                // 餌を食べる処理実施
                 feed.PickUp();
-
                 PlayerEvolution.Instance.EatFeed(feed);
             }
+
+            // TODO : 敵の判定処理実装
         }
     }
 }
