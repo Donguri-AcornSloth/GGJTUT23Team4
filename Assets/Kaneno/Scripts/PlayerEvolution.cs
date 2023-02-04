408ec894639f215612e9e0231b80aba2efa5f73d
ftvoid <gensaicuttlefish@gmail.com>
2023-02-04 12:19:14 +0900

    プレイヤーの進化情報の管理クラス（PlayerEvolution）のプロパティを仮実装。

--- Assets/Kaneno/Scripts/PlayerEvolution.cs
+++ Assets/Kaneno/Scripts/PlayerEvolution.cs
@@ -1,12 +1,56 @@
+using System;
 using UnityEngine;
 
 namespace Player
 {
     public class PlayerEvolution : MonoBehaviour, IInitialize
     {
+        public static PlayerEvolution Instance { get; private set; }
+
+        #region 初期化
+
         public void Initialize()
         {
             // TODO : 初期化処理などを実装していく
+        }
+
+        #endregion
+
+        #region パラメータ
+
+        /// <summary>
+        /// 進化の段階
+        /// </summary>
+        public int Level { get; private set; }
+
+        /// <summary>
+        /// 進化までのゲージ割合(0～1)
+        /// 1になったら次の段階に進化する
+        /// </summary>
+        public float Percentage { get; private set; }
+
+        public enum EvolutionType
+        {
+            // なし
+            None,
+
+            // 草食
+            Herbivore,
+
+            // 肉食
+            Carnivorous,
+
+            // 雑食
+            Omnivorous,
+        }
+
+        public EvolutionType Type { get; private set; } = EvolutionType.None;
+
+        #endregion
+
+        private void Awake()
+        {
+            Instance = this;
         }
     }
 }
---
+++ Assets/Kaneno/Tests/TestPlayerEvolution.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player.Tests
+{
+    public class TestPlayerEvolution : MonoBehaviour
+    {
+        private void Update()
+        {
+            var keyboard = Keyboard.current;
+            if (keyboard == null) return;
+
+            // デバッグでプレイヤーの進化情報を出力
+            if (keyboard.enterKey.wasPressedThisFrame)
+            {
+                Debug.Log($"PlayerEvolution.Instance : {PlayerEvolution.Instance}");
+                Debug.Log($"PlayerEvolution.Instance.Level : {PlayerEvolution.Instance.Level}");
+                Debug.Log($"PlayerEvolution.Instance.Percentage : {PlayerEvolution.Instance.Percentage}");
+                Debug.Log($"PlayerEvolution.Instance.Type : {PlayerEvolution.Instance.Type}");
+            }
+        }
+    }
+}
