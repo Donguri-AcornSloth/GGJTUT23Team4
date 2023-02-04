7c100cd5bb6530831792600996e72a455fa758b7
shu13522 <m0a201984b@edu.teu.ac.jp>
2023-02-05 02:59:19 +0900

    EnemyGenerator割と完成

--- Assets/Nakamura/Scripts/Enemy_02.cs
+++ Assets/Nakamura/Scripts/Enemy_02.cs
@@ -1,9 +1,10 @@
+using Player;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
+using static EnemyBase;
 
-public class Enemy_01 : EnemyBase
+public class Enemy_02 : EnemyBase
 {
     [SerializeField]
     private float _moveSpeed = 1;
@@ -23,8 +24,16 @@ public class Enemy_01 : EnemyBase
     // Update is called once per frame
     void FixedUpdate()
     {
-        UpdateState();
+        if(InCircle(transform.position, _player.gameObject.transform.position, _raderRadius))
+        {
+            if (_stateManager.CurrentStateType == ActionStateEnum.Chase) return;
+
+            _stateManager.ForceStop();
+            _stateManager.AddState(ActionStateEnum.Chase);
+        }
+
         DeathspownTimerUpdate();
+        UpdateState();
     }
 
     protected override void SelectActionState()
@@ -43,5 +52,15 @@ public class Enemy_01 : EnemyBase
         _moveTimer.Update();
 
         return _moveTimer.IsTimeUp;
+    }
+    protected override void OnStateTurnEnter()
+    {
+        _turnTimer.ResetTimer(_turnTime);
+        _turnRate = 0;
+
+        _turnTimer.ResetTimer(_turnTime);
+        _turnRate = 0;
+
+        _targetRotation = Quaternion.AngleAxis(Vector3.Angle(_player.gameObject.transform.position, gameObject.transform.position), Vector3.forward) * transform.rotation;
     }
 }
