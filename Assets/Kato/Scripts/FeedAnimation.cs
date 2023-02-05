2993d3ba436f1241d918fb15209326788da0eff8
ftvoid <gensaicuttlefish@gmail.com>
2023-02-05 16:14:08 +0900

    餌のフェードイン演出を追加。

--- Assets/Kato/Scripts/FeedAnimation.cs
+++ Assets/Kato/Scripts/FeedAnimation.cs
@@ -6,8 +6,14 @@ public class FeedAnimation : MonoBehaviour
     [SerializeField] private float _amplitudeRot;
     [SerializeField] private float _speed;
 
+    [SerializeField] private float _fadeInDuration = 1;
+    
     private Vector2 _noisePos;
     private Vector3 _initialPosition;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private float _fadeAlpha;
 
     private void Start()
     {
@@ -17,10 +23,13 @@ public class FeedAnimation : MonoBehaviour
         );
 
         _initialPosition = transform.position;
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        // 揺らぎの動き
         _noisePos += Vector2.one * _speed * Time.deltaTime;
 
         var noiseValue = new Vector2(
@@ -30,5 +39,14 @@ public class FeedAnimation : MonoBehaviour
 
         transform.position = _initialPosition + (Vector3) noiseValue * _amplitude;
         transform.rotation = Quaternion.Euler(0, 0, noiseValue.x * _amplitudeRot);
+
+        // フェードイン演出
+        if (_fadeAlpha < 1)
+        {
+            _fadeAlpha += Time.deltaTime / _fadeInDuration;
+            var color = _spriteRenderer.color;
+            color.a = Mathf.Clamp01(_fadeAlpha);
+            _spriteRenderer.color = color;
+        }
     }
 }
